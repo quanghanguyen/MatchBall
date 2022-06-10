b2a26bdc87a970dc1484d2e7e63a9c3413f29177
Quang Ha <hakun32@gmail.com>
2022-06-10 11:40:23 +0700

    search-1006

--- app/src/main/java/com/example/matchball/home/MainActivity.kt
+++ app/src/main/java/com/example/matchball/home/MainActivity.kt
@@ -4,18 +4,20 @@ package com.example.matchball.home
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.viewModels
 import androidx.fragment.app.Fragment
 import com.example.matchball.R
 import com.example.matchball.createrequest.RequestActivity
 import com.example.matchball.databinding.ActivityMainBinding
 import com.example.matchball.dashboard.MatchListFragment
+import com.example.matchball.firebaseconnection.AuthConnection.authUser
 import com.example.matchball.usersetting.UserFragment
 
-// Sửa UI :
-// 5/ activity_user_account : avatar
-// 6/ activity_user_info : avatar
-// 7/ fragment_user
-
+/* Sửa UI :
+ 5/ activity_user_account : avatar
+ 6/ activity_user_info : avatar
+ 7/ fragment_user */
 
 // Xác thực email
 // get email và ảnh của current User
@@ -23,12 +25,15 @@ import com.example.matchball.usersetting.UserFragment
 
 //------------------------------
 
-// Notification
 // Search
+// Notification
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var mainBinding: ActivityMainBinding
+    private val mainActivityViewModel : MainActivityViewModel by viewModels()
+    private var name : String? = null
+    private var phone : String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,7 +41,21 @@ class MainActivity : AppCompatActivity() {
         setContentView(mainBinding.root)
 
         initEvents()
+        initObserve()
         initUI()
+        mainActivityViewModel.handleReadUserData()
+
+    }
+
+    private fun initObserve() {
+        mainActivityViewModel.userInfo.observe(this, {result ->
+            when (result) {
+                is MainActivityViewModel.UserData.LoadDataOk -> {
+                    name = result.name
+                    phone = result.phone
+                }
+            }
+        })
     }
 
     private fun initUI() {
@@ -64,9 +83,14 @@ class MainActivity : AppCompatActivity() {
                     loadFragment(fragment)
                     true
                 }
+                // check uid is exist
                 R.id.request -> {
-                    startActivity(Intent(applicationContext, RequestActivity::class.java))
-                    overridePendingTransition(0, 0)
+                    if (authUser!!.isEmailVerified && name!!.isNotEmpty() && phone!!.isNotEmpty()) {
+                        startActivity(Intent(applicationContext, RequestActivity::class.java))
+                        overridePendingTransition(0, 0)
+                    } else {
+                        Toast.makeText(this, "Please complete your Profile", Toast.LENGTH_SHORT).show()
+                    }
                     true
                 }
                 else -> false
